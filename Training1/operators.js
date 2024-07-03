0d8fc97d61b561c3af306f96e15f006c610831b8
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-03 11:46:57 +0530

    Add files via upload

---
+++ Training1/operators.js
@@ -0,0 +1,57 @@
+let a=2;
+let b=3;
+
+//arithmetic
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+//logical
+console.log(a>5 && b<5);
+console.log(a>5 || b>5);
+console.log(!(a>5));
+
+//assignment
+let c=5;
+c+=3;
+console.log(c);
+
+//comparison
+console.log(a>b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a<b);
+
+
+//ternary
+let age=18;
+let vote=(age>=18)?'yes':'no';
+console.log(vote);
+
+//increment
+let count =5;
+console.log(count++);
+console.log(count--);
+console.log(count);
+console.log(--count);
+console.log(++count);
+console.log(count);
+//string
+let fname="sree";
+let course="js";
+console.log(fname  ,  course);
+
+//bitwise
+let value1=4;
+let value2=5;
+console.log(value1&value2);
+console.log(value1|value2);
+console.log(value1^value2);
+console.log(~value1);
+console.log(value1<<value2);
+console.log(value1>>value2);
+console.log(value1>>value2);
