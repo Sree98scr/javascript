1878600bd3f43bc7d94165da29eed3823bfe0bbd
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-03 11:33:31 +0530

    Add files via upload

--- Training1/variables.js
+++ Training1/variables.js
@@ -1,1 +1,28 @@
 
+//var
+var a=2;
+if(a){
+    var a=4;
+    console.log(a);
+}
+console.log(a);
+
+//let
+let b=1;
+if(b){
+    let b=2;
+    console.log(b);
+}
+console.log(b);
+if(b){
+    let b=3;
+    console.log(b);
+}
+console.log(b);
+
+//const
+const c=5;
+if(c){
+    c==4;
+    console.log(c);
+}
