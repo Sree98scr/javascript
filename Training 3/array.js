9873ec2c3daf82125f831a916b39c23074ea28d8
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-04 14:21:59 +0530

    add file via upload

---
+++ Training 3/array.js
@@ -0,0 +1,23 @@
+//array
+
+//accessing array;
+let fruits= ["apple","orange","grape"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+//modify element
+fruits[1]="banana";
+console.log(fruits[1]);
+
+//array length
+console.log(fruits.length);
+//adding element
+
+fruits.push("cheery");
+console.log(fruits);
+//removing
+
+let lastfruits=fruits.pop
+console.log(lastfruits);
+console.log(fruits);
