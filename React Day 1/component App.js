bb901ea91046cf051ef48039e0bb63ed8e23522a
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-10 21:43:46 +0530

    file

---
+++ React Day 1/component App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MountingComponent from './Components/mounting.js'; 
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <MountingComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
