f669b5c7c333970686b29cd3336bb07fe43923b2
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-17 14:37:54 +0530

    Create ref.js

---
+++ React day 3 /ref.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Ref from './useref/ref';
+
+function Apply(){
+    return(
+        <div>
+            <Ref/><br></br><br></br>
+        
+        </div>
+    )
+}
+export default Apply
