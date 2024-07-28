93e4f9774ce3bf69effba25d2addcd0df6b2555b
Sree98scr <sreelakshmi0067@gmail.com>
2024-07-29 05:26:07 +0530

    Add files via upload

--- Task Management/index.js
+++ Task Management/index.js
@@ -1,43 +1,18 @@
-'use strict';
+const express= require('express');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const app = express();
+const cors= require('cors');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+app.use(express.json());
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+app.use(cors());
+ const db= require('./models/index');
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+ const taskRouter=require('./routes/Task');
+ app.use('/tasks',taskRouter);
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+ db.sequelize.sync().then(() =>{
+    app.listen(3001, () => {
+        console.log('Server is running on port 3001');  // server is running on port 3001
+     }); 
+ })
